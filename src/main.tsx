c2933c4924b2d9f46a084d2afd79e512da76b478
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2025-07-22 15:38:14 +0700

    feat: addd

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import './styles/index.css';
-import { SidebarProvider } from './contexts/SidebarContext.tsx';
-import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <ThemeProvider>
-      <SidebarProvider>
-        <App />
-      </SidebarProvider>
-    </ThemeProvider>
+    <App />
   // </StrictMode>,
 )
